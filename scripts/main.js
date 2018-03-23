e3b1fcaf6f68717b7cbae75b91c0298eb4b2b72e
Nma Mbeledogu <ncmbeledogu@gmail.com>
2018-03-23 10:15:04 -0700

    Create menu, but styling is incomplete

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,19 @@
+function myFunction(x) {
+    x.classList.toggle("change");
+}
 
+/* Set the width of the side navigation to 250px */
+function toggleNav() {
+  var sideNav = document.getElementById("mySidenav");
+
+  if (sideNav.style.width == "250px") {
+    sideNav.style.width = "0";
+  }
+  // hotfix because width == "" when page first loaded for some reason
+  else if (sideNav.style.width == "") {
+    sideNav.style.width = "250px";
+  }
+  else if (sideNav.style.width == "0px") {
+    sideNav.style.width = "250px";
+  }
+}
