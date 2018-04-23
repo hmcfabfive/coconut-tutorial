14c5f7e2d9b02333844e93e65033ea07bcdad1bf
minierm97 <minierm97@gmail.com>
2018-04-22 21:59:29 -0700

    Fixed Data Types code error

--- scripts/codesnippets.js
+++ scripts/codesnippets.js
@@ -18,10 +18,10 @@ var ite12 = "def x(n) = (n,) :: x(n + 1)"
 var ite13 = ite12 + "\n\n" + "x(5)$[0:5] |> list |> print"
 
 /** start of datatypes code snippets */
-var dat1 = "data Leaf(n)\ndata Node(l, r)"
-var dat2 = dat1 + "\n\n" + "data Empty():\n    def size(self):\n      return 0"
+var dat1 = "data Empty()\ndata Leaf(n)\ndata Node(l, r)"
+var dat2 = dat1 + "\n\n" + "def size(Empty()) = 0"
 var dat3 = dat2 + "\n\n" + "@addpattern(size)\ndef size(Leaf(n)) = 1\n\n@addpattern(size)\ndef size(Node(l, r)) = size(l) + size(r)"
-var dat4 = dat3 + "\n\n" + "size(Node(Empty(), Leaf(10)))"
+var dat4 = dat3 + "\n\n" + "size(Node(Empty(), Leaf(10))) |> print"
 
 /** start of recursive factorial code snippets */
 var rec1 = "def factorial(n):\n    \"\"\"Compute n! where n is an integer >= 0.\"\"\"\n\n#Test cases:\n0 |> factorial |> print  # 1"
